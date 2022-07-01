145ff0fe7d5f7d4c9718da83ae84660d77adfda6
Dhenxdy7 <dhenxdy7@gmail.com>
2022-07-01 16:43:08 +0700

    first commit

--- AntriRS.cpp
+++ AntriRS.cpp
@@ -1,157 +1,162 @@
 #include <iostream>
 #include <conio.h>
-#include <windows.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
 using namespace std;
-int main ()
+
+int nomer[MAX];
+int head=-1;
+int tail=-1;
+
+//Fungsi Untuk pengecekan antrian
+bool IsEmpty()
 {
-char penyakit;
-char nama[50];
-char alamat[75];
-int dokter;
-int lama;
-int kamar;
-char kelamin[25];
-int harga;
-int jasa;
-char lagi;
-int dibayar;
-int total;
-int kembali;
+  if(tail == -1)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+bool IsFull()
+{
+  if(tail == MAX-1)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
 
+//Fungsi untuk memasukan antrian (queue) kedalam array
+void AntrianMasuk(int no)
+{
+  if(IsEmpty())
+  {
+    head=tail=0;
+  }
+  else
+  {
+    tail++;
+  }
+  nomer[tail]=no;
+}
 
-awal:
-cout<<"++++++++++++++++++++++++++++++++++++++++++++"<<endl;
-cout<<"========Rumah Sakit Amikom==========="<<endl;
-cout<<"++++++++++++++++++++++++++++++++++++++++++++"<<endl;
-cout<<"        Masukkan Data Anda Dibawah          "<<endl;
-cout<<endl;
-
-    cout<<"masukkan nama anda       =";
-    cin>>nama;
-    cout<<endl;
-
-    cout<<"Masukkan Alamat Anda     =";
-    cin>>alamat;
-    cout<<endl;
-
-    cout<<"Masukkan Jenis Kelamin   =";
-    cin>>kelamin;
-    cout<<endl;
-
-    cout<<"Masukkan Lama Menginap   =";
-    cin>>lama;
-    cout<<endl;
-
-    cout<<" Jenis Kamar "<<endl;
-    cout<<" 1.Kelas I   "<<endl;
-    cout<<" 2.Kelas II  "<<endl;
-    cout<<" 3.Kelas III "<<endl;
-    cout<<" 4.Kelas VIP "<<endl;
-    cout<<" 5.Kelas VVIP"<<endl;
-    cout<<"Pilih Jenis Kamar Anda[1-5] =";
-    cin>>kamar;
-
-        switch(kamar)
-        {
-
-    case 1:
-     harga=800000;
-     cout<<"Kelas I"<<endl;
-     cout<<"Tarif 800000"<<endl;
-     break;
-
-    case 2:
-     harga=600000;
-     cout<<"Kelas II"<<endl;
-     cout<<"Tarif 800000"<<endl;
-     break;
-
-    case 3:
-     harga=300000;
-     cout<<"Kelas III"<<endl;
-     cout<<"Tarif 800000"<<endl;
-     break;
-
-    case 4:
-     harga=800000;
-     cout<<"Kelas VIP"<<endl;
-     cout<<"Tarif 1300000"<<endl;
-     break;
-
-    case 5:
-     harga=1500000;
-     cout<<"Kelas VVIP"<<endl;
-     cout<<"Tarif 150000"<<endl;
-     break;
-
-    default:
-    cout<<"Tidak Tersedia";
+//Fungsi untuk menghapus antrian
+void AntrianKeluar()
+{
+  if(IsEmpty())
+  {
+    cout<<"Antrian sudah kosong ! ";
+    getch();
+  }
+  else
+  {
+    for(int a=head;a<tail;a++)
+    {
+      nomer[a]=nomer[a=1];
     }
-        cout<<endl;
-
-        cout<<" Dokter "<<endl;
-        cout<<" 1.Dokter Anak"<<endl;
-        cout<<" 2.Dokter Penyakit Dalam"<<endl;
-        cout<<" 3.Dokter THT"<<endl;
-        cout<<" 4.Dokter Bedah"<<endl;
-        cout<<" 5.Dokter Umum"<<endl;
-        cout<<"Masukkan Pilihan Anda[1-5] =";
-        cin>>dokter;
-
-    switch(dokter){
-    case 1:
-      jasa=70000;
-      cout<<"Dokter Anak"<<endl;
-      cout<<"Tarif 70000"<<endl;
-      break;
-
-    case 2:
-      jasa=80000;
-      cout<<"Dokter Penyakit Dalam"<<endl;
-      cout<<"Tarif 80000"<<endl;
-      break;
-
-    case 3:
-      jasa=60000;
-      cout<<"Dokter THT"<<endl;
-      cout<<"Tarif 60000"<<endl;
-      break;
-
-    case 4:
-      jasa=100000;
-      cout<<"Dokter Bedah"<<endl;
-      cout<<"Tarif 100000"<<endl;
-      break;
-
-    case 5:
-      jasa=50000;
-      cout<<"Dokter Umum"<<endl;
-      cout<<"Tarif 50000"<<endl;
-      break;
-
-    default:
-        cout<<"Tidak Tersedia"<<endl;
+    tail--;
+    if(tail ==- 1)
+    {
+      head = -1;
     }
+  }
+}
 
-    cout<<"++++++++++DATA ANDA++++++++++"<<endl;
-    cout<<"Nama Anda            :"<<nama<<endl;
-    cout<<"Alamat Anda          :"<<alamat<<endl;
-    cout<<"Jenis Kelamin        :"<<kelamin<<endl;
-    cout<<"Lama Menginap        :"<<lama<<endl;
-    cout<<"Jenis Kamar          :"<<kamar<<endl;
-    cout<<"Dokter               :"<<dokter<<endl;
-    cout<<"Yang Harus Dibayar   :"<<jasa+harga<<endl;
-cout<<endl;
-cout<<"Ingin Mengulangi Lagi (Y/N)?";
-cin>>lagi;
-    if(lagi=='y' || lagi=='Y'){
-        goto awal;
-    }
-    if(lagi=='n' || lagi=='N'){
-            goto selesai;
-    }
-    selesai:
-        cout<<"Terima Kasih Atas Kunjungan Anda di Rumah Sakit Amikom"<<endl;
+//Fungsi untuk menghapus semua antrian
+void Clear()
+{
+  head=tail=-1;
+}
 
-return 0;
+//Fungsi untuk melihat semua antrian
+void View()
+{
+  if(IsEmpty())
+  {
+    cout<<"Antrian kosong ! ";
+  }
+  else
+  {
+    system("cls");
+    for(int a=head;a<=tail;a++)
+    {
+      cout << "========================================="
+           << "\n >> No. Antri : [" << nomer[a] << "]"
+           << "\n========================================" << endl;
+    }
+  }
+}
+
+int main()
+{
+  system("color 79");
+  int choose, p=1, urut;
+  do
+  {
+    system("cls");
+    cout << "\n\n===== PROGRAM ANTRIAN C++ ====="
+         << "\n================================"
+         << "\n|1. Tambah Antrian              "
+         << "\n|2. Panggil Antrian             "
+         << "\n|3. Lihat daftar antrian        "
+         << "\n|4. Format                      "
+         << "\n|5. Exit                        "
+         << "\n|===============================";
+    cout << "\nChoose ! "; cin >> choose;
+    cout << "\n\n";
+    if(choose == 1)
+    {
+      if(IsFull())
+      {
+        cout << "Antrian sudah penuh, mohon tunggu beberapa saat lagi";
+      }
+      else
+      {
+        urut=p;
+        AntrianMasuk(urut);
+        cout << "-----------------------------------" << endl;
+        cout << "|           NO. ANTRIAN           |" << endl;
+        cout << "|               " << p << "               ||" << endl;
+        cout << "-----------------------------------" << endl;
+        cout << "|        Silahkan Mengantri       |" << endl;
+        cout << "|       Menunggu " << tail << " Antrian       ||" << endl;
+        cout << "-----------------------------------" << endl;
+        p++;
+      }
+    } 
+    else if(choose ==2)
+    {
+      cout << "-----------------------------------" << endl;
+      cout << "No. Antri : ["<< nomer[head] <<    "]";
+      cout << "\n=================================" << endl;
+      AntrianKeluar();
+      cout << "Silahkan Dipanggil !" << endl;
+    }
+    else if(choose ==3)
+    {
+      View();
+    } 
+    else if(choose ==4)
+    {
+      Clear();
+      cout << "Antrian dikosongkan ! ";
+    }
+    else if(choose ==5)
+    { 
+    }
+    else
+    {
+      cout << "Masukan anda salah! \n" << endl;
+    }
+    getch();
+  }
+  while(choose!=5);
 }
